2f2a4e24a6192c07261502a49b98c8a2c9934836
varun <ballaabhinav2@gmsil.com>
2024-10-01 18:25:59 +0530

    My first python programe

---
+++ rightangle.py
@@ -0,0 +1,8 @@
+def print_right_angle_triangle(height):
+    for i in range(1, height + 1):
+        print('*' * i)
+
+# Example usage:
+height = int(input("Enter the height of the triangle: "))
+print_right_angle_triangle(height)
+
